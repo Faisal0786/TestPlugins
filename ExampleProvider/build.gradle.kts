6c3bcf8f54e3afae6ff29227e15bd4f8deefe03e
Faisal <39812782+Faisal0786@users.noreply.github.com>
2026-05-14 03:30:57 +0530

    Update build.gradle.kts

--- ExampleProvider/build.gradle.kts
+++ ExampleProvider/build.gradle.kts
@@ -1,26 +1,46 @@
-dependencies {
-    implementation("com.google.android.material:material:1.12.0")
-    implementation("androidx.recyclerview:recyclerview:1.3.2")
-}
+import org.jetbrains.kotlin.konan.properties.Properties
 
-version = 200
-
-cloudstream {
-    description = "Faisal's Premium Extension"
-    authors = listOf("Faisal0786")
-    status = 1
-    tvTypes = listOf(
-        "Movie",
-        "TvSeries"
-    )
-    
-    language = "en"
-    iconUrl = "https://streamimdb.ru/favicon.ico"
-}
+version = 1
 
 android {
-    buildFeatures {
-        buildConfig = true
-        viewBinding = true
+
+    defaultConfig {
+
+        val properties = Properties()
+
+        properties.load(
+            project.rootProject
+                .file("local.properties")
+                .inputStream()
+        )
+
+        android.buildFeatures.buildConfig = true
+
+        buildConfigField(
+            "String",
+            "TMDB_KEY",
+            "\"${properties.getProperty("TMDB_KEY")}\""
+        )
     }
+}
+
+cloudstream {
+
+    description = "StreamIMDB Premium Provider"
+
+    authors = listOf(
+        "Faisal0786"
+    )
+
+    status = 1
+
+    tvTypes = listOf(
+        "TvSeries",
+        "Movie",
+        "AsianDrama",
+        "Anime",
+        "Torrent"
+    )
+
+    iconUrl = "https://streamimdb.ru/favicon.ico"
 }
