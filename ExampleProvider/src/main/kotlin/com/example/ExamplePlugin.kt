ed42afad2b3de20dafcff3f9bf3c6ef5146c32fb
Faisal <39812782+Faisal0786@users.noreply.github.com>
2026-05-15 02:52:00 +0530

    Update ExamplePlugin.kt

--- ExampleProvider/src/main/kotlin/com/example/ExamplePlugin.kt
+++ ExampleProvider/src/main/kotlin/com/example/ExamplePlugin.kt
@@ -3,7 +3,7 @@ package com.example
 import com.lagradost.cloudstream3.plugins.*
 
 @CloudstreamPlugin
-class ExamplePlugin : Plugin() {
+class StreamImdbPlugin : Plugin() {
     override fun load() {
         registerMainAPI(StreamImdbProvider())
     }
