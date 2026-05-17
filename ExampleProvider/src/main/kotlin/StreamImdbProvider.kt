b4e1e9932c32493bd4746c441eb93055f0f6eda5
Faisal <39812782+Faisal0786@users.noreply.github.com>
2026-05-17 15:54:12 +0530

    Update StreamImdbProvider.kt

--- ExampleProvider/src/main/kotlin/StreamImdbProvider.kt
+++ ExampleProvider/src/main/kotlin/StreamImdbProvider.kt
@@ -1,8 +1,7 @@
 package com.example
 
 import com.lagradost.cloudstream3.*
-import com.lagradost.cloudstream3.LoadResponse.Companion.addTrailer
-import 
+import com.lagradost.cloudstream3.LoadResponse.Companion.addTrailer 
 import com.lagradost.cloudstream3.app
 import com.lagradost.cloudstream3.utils.*
 import com.lagradost.cloudstream3.utils.AppUtils.toJson
