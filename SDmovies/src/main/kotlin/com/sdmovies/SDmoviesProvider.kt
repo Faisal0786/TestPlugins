cae839c629bac46362ad3c1df66ddd46ade3036e
Faisal <39812782+Faisal0786@users.noreply.github.com>
2026-05-18 16:56:46 +0530

    Update SDmoviesProvider.kt

--- SDmovies/src/main/kotlin/com/sdmovies/SDmoviesProvider.kt
+++ SDmovies/src/main/kotlin/com/sdmovies/SDmoviesProvider.kt
@@ -1,38 +1,37 @@
 package com.sdmovies
 
 import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
-import com.lagradost.cloudstream3.api.SubtitleFile
+import com.lagradost.cloudstream3.MainAPI
+import com.lagradost.cloudstream3.SearchResponse
+import com.lagradost.cloudstream3.LoadResponse
+import com.lagradost.cloudstream3.newMovieSearchResponse
+import com.lagradost.cloudstream3.newMovieLoadResponse
+import com.lagradost.cloudstream3.HomePageResponse
+import com.lagradost.cloudstream3.HomePageList
+import com.lagradost.cloudstream3.MainPageRequest
+import com.lagradost.cloudstream3.TvType
 import com.lagradost.cloudstream3.utils.ExtractorLink
-import com.lagradost.cloudstream3.api.MainAPI
-import com.lagradost.cloudstream3.api.SearchResponse
-import com.lagradost.cloudstream3.api.LoadResponse
-import com.lagradost.cloudstream3.api.MovieSearchResponse
-import com.lagradost.cloudstream3.api.MovieLoadResponse
-import com.lagradost.cloudstream3.api.HomePageResponse
-import com.lagradost.cloudstream3.api.HomePageList
-import com.lagradost.cloudstream3.api.MainPageRequest
-import com.lagradost.cloudstream3.api.TvType
-import com.lagradost.cloudstream3.utils.Qualities
+import com.lagradost.cloudstream3.utils.newExtractorLink
 import org.jsoup.Jsoup
 
 @CloudstreamPlugin
-class SDmoviesProvider: MainAPI() {
+class SDmoviesProvider : MainAPI() {
     override val mainUrl = "https://sdmoviespoint.cyou"
     override val name = "SDmovies"
     override val hasMainPage = true
-    
+
     override val mainPageUrlList = listOf(
         Pair("Latest", "$mainUrl/"),
         Pair("Movies", "$mainUrl/latest-movies"),
         Pair("Web Series", "$mainUrl/web-series")
     )
-    
+
     override suspend fun getMainPage(
         page: Int,
         request: MainPageRequest
     ): HomePageResponse {
         val list = mutableListOf<HomePageList>()
-        
+
         mainPageUrlList.forEach { (name, url) ->
             try {
                 val doc = Jsoup.connect(url).get()
@@ -40,9 +39,9 @@ class SDmoviesProvider: MainAPI() {
                     val title = element.select("h2.post-title").text()
                     val link = element.select("a").attr("href")
                     val poster = element.select("img").attr("src")
-                    
+
                     if (title.isNotEmpty() && link.isNotEmpty()) {
-                        MovieSearchResponse(
+                        newMovieSearchResponse(
                             name = title,
                             url = link,
                             apiName = this.name,
@@ -51,7 +50,7 @@ class SDmoviesProvider: MainAPI() {
                         )
                     } else null
                 }
-                
+
                 if (items.isNotEmpty()) {
                     list.add(HomePageList(name, items))
                 }
@@ -59,21 +58,21 @@ class SDmoviesProvider: MainAPI() {
                 e.printStackTrace()
             }
         }
-        
+
         return HomePageResponse(list)
     }
-    
+
     override suspend fun search(query: String): List<SearchResponse> {
         return try {
             val doc = Jsoup.connect("$mainUrl/?s=$query").get()
-            
+
             doc.select("div.post-box").mapNotNull { element ->
                 val title = element.select("h2.post-title").text()
                 val link = element.select("a").attr("href")
                 val poster = element.select("img").attr("src")
-                
+
                 if (title.isNotEmpty() && link.isNotEmpty()) {
-                    MovieSearchResponse(
+                    newMovieSearchResponse(
                         name = title,
                         url = link,
                         apiName = this.name,
@@ -87,42 +86,46 @@ class SDmoviesProvider: MainAPI() {
             emptyList()
         }
     }
-    
+
     override suspend fun load(url: String): LoadResponse? {
         return try {
             val doc = Jsoup.connect(url).get()
-            
+
             val title = doc.select("h1.post-title").text()
             val poster = doc.select("img.post-image").attr("src")
             val description = doc.select("div.post-content p").text()
-            
-            MovieLoadResponse(
+
+            newMovieLoadResponse(
                 name = title,
                 url = url,
-                apiName = this.name,
                 type = TvType.Movie,
-                posterUrl = poster,
-                plot = description,
-                tags = emptyList()
-            )
+                dataUrl = url
+            ) {
+                this.posterUrl = poster
+                this.plot = description
+            }
         } catch (e: Exception) {
             e.printStackTrace()
             null
         }
     }
-    
-    override suspend fun getVideoLinks(url: String): List<ExtractorLink> {
+
+    override suspend fun loadLinks(
+        data: String,
+        isCasting: Boolean,
+        subtitleCallback: (SubtitleFile) -> Unit,
+        callback: (ExtractorLink) -> Unit
+    ): Boolean {
         return try {
-            val doc = Jsoup.connect(url).get()
-            val links = mutableListOf<ExtractorLink>()
-            
+            val doc = Jsoup.connect(data).get()
+
             doc.select("a.download-link").forEach { element ->
                 val linkUrl = element.attr("href")
                 val quality = element.text()
-                
+
                 if (linkUrl.isNotEmpty()) {
-                    links.add(
-                        ExtractorLink(
+                    callback(
+                        newExtractorLink(
                             source = this.name,
                             name = quality,
                             url = linkUrl,
@@ -133,11 +136,11 @@ class SDmoviesProvider: MainAPI() {
                     )
                 }
             }
-            
-            links
+
+            true
         } catch (e: Exception) {
             e.printStackTrace()
-            emptyList()
+            false
         }
     }
 }
